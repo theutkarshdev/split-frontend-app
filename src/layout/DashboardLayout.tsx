37736a191af17cc886da0c23ec40951ecdc4d459
Utkarsh Kushwaha <theutkarshdev@gmail.com>
2025-09-14 20:08:56 +0530

    corners-smooth

--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -15,7 +15,7 @@ const DashboardLayout = () => {
       </main>
 
       <div className="fixed bottom-5 w-full px-5 max-w-xs">
-        <div className="bg-primary p-3 text-white squircle rounded-4xl flex justify-around">
+        <div className="bg-primary p-3 text-white squircle rounded-xl flex justify-around">
           <HomeIcon />
           <PlusCircleIcon />
           <BellDotIcon />
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
   return (
     <div>
       <h1 className="text-xl font-semibold mb-3">Hello, Good Morning..! </h1>
-      <div className="w-full h-40 bg-slate-200 border rounded-[3rem] squircle p-5">
+      <div className="w-full h-40 bg-slate-200 border rounded-xl squircle p-5">
         Card
       </div>
       <div className="mt-4 relative">
@@ -18,7 +18,7 @@ const Dashboard = () => {
 
       <div className="grid grid-cols-3 gap-2 mt-4">
         {[...Array(9)].map(() => (
-          <div className="w-full aspect-square bg-slate-200 rounded-[3rem] squircle grid place-content-center">
+          <div className="w-full aspect-square bg-slate-200 rounded-xl squircle grid place-content-center">
             <img
               className="rounded-full size-20 border-2 border-slate-300"
               src="https://avatar.iran.liara.run/public"
