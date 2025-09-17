4fe2b9b3e8794f13019858a773a9a3c1e1081640
Pinku Rathour <pinkurathour2016@gmail.com>
2025-09-17 13:59:40 +0530

    resolved conflict error

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -11,13 +11,8 @@ const AuthLayout = () => {
   }
 
   return (
-<<<<<<< HEAD
     <div className="bg-black flex justify-center">
       <main className="w-full min-h-screen px-5 max-w-md bg-white">
-=======
-    <div className="flex justify-center overflow-x-hidden">
-      <main className="w-full min-h-screen px-5 bg-white">
->>>>>>> e024afe3d043844ab5adef82dbd10ff5a434dcc4
         <Outlet />
       </main>
     </div>
