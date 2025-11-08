4a829b3f0f1439e66923950d9cceca99f279a7e3
Npkrathour <pinkurathour.dev@gmail.com>
2025-11-08 10:59:05 +0530

    bug fixes

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -56,10 +56,10 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <PageLayout>
-      <div className="h-[40vh]">
+    <PageLayout className="p-0">
+      <div className="h-[40vh] relative">
         <CustomCard
-          radius={40}
+          radius={35}
           className="p-5 h-full"
           pClassName="absolute h-[calc(40vh+3rem)] w-full right-5 translate-x-5 -top-8 p-0"
         >
@@ -81,80 +81,85 @@ const Dashboard = () => {
         </CustomCard>
       </div>
 
-      <CustomCard className="p-5" pClassName="mt-5" radius={20}>
-        <div className="flex justify-between items-center mb-3">
-          <h2 className="text-lg font-semibold">My Friends</h2>
-          <Button
-            variant="ghost"
-            className="p-0 opacity-50 h-5"
-            onClick={() => navigate("/search?friend_filter=friends")}
-          >
-            See All
-          </Button>
-        </div>
-
-        <div className="mt-4">
-          <div className="grid grid-cols-5 gap-2">
-            {/* Add Button (common for both states) */}
-            <div
-              className="text-center cursor-pointer"
-              onClick={() => navigate("/search?friend_filter=all")}
+      <div className="p-5">
+        <CustomCard className="p-5" pClassName="mt-5" radius={25}>
+          <div className="flex justify-between items-center mb-3">
+            <h2 className="text-lg font-semibold">My Friends</h2>
+            <Button
+              variant="ghost"
+              className="p-0 opacity-50 h-5"
+              onClick={() => navigate("/search?friend_filter=friends")}
             >
-              <CustomCard radius={13} className="w-full aspect-square p-2">
-                <PlusIcon className="size-full text-zinc-200 dark:text-zinc-700" />
-              </CustomCard>
-              <div className="overflow-hidden mt-2">
-                <h3 className="text-xs font-medium truncate">Add</h3>
-              </div>
-            </div>
-
-            {/* Loading State */}
-            {loading &&
-              [...Array(4)].map((_, idx) => (
-                <div key={idx}>
-                  <CustomCard radius={13} className="w-full aspect-square">
-                    <Skeleton className="size-full" />
-                  </CustomCard>
-                  <Skeleton className="h-3 w-[90%] mt-2 mx-auto" />
-                </div>
-              ))}
-
-            {/* Loaded State */}
-            {!loading &&
-              !error &&
-              profiles.slice(0, 4).map((item, idx) => (
-                <div
-                  key={idx}
-                  className="text-center cursor-pointer"
-                  onClick={() => {
-                    const status = item.friend_request_status;
-                    if (status === undefined || status === "accepted") {
-                      navigate(`/activity/${item.id}`);
-                    }
-                  }}
-                >
-                  <CustomCard radius={13} className="w-full aspect-square">
-                    <img
-                      className="size-full aspect-square object-cover"
-                      src={item.profile_pic || AvtarImg}
-                      alt={item.username}
-                      loading="lazy"
-                      onError={(e) => {
-                        e.currentTarget.onerror = null;
-                        e.currentTarget.src = AvtarImg;
-                      }}
-                    />
-                  </CustomCard>
-                  <div className="overflow-hidden mt-2">
-                    <h3 className="text-xs font-medium truncate">
-                      @{item.username}
-                    </h3>
-                  </div>
-                </div>
-              ))}
+              See All
+            </Button>
           </div>
-        </div>
-      </CustomCard>
+
+          <div className="mt-4">
+            <div className="grid grid-cols-4 gap-2">
+              {/* Add Button (common for both states) */}
+              <div
+                className="text-center cursor-pointer"
+                onClick={() => navigate("/search?friend_filter=all")}
+              >
+                <CustomCard
+                  radius={50}
+                  className="w-full aspect-square grid place-content-center"
+                >
+                  <PlusIcon className="size-10 p-0 text-zinc-200 dark:text-zinc-700" />
+                </CustomCard>
+                <div className="overflow-hidden mt-2">
+                  <h3 className="text-xs font-medium truncate">Add</h3>
+                </div>
+              </div>
+
+              {/* Loading State */}
+              {loading &&
+                [...Array(7)].map((_, idx) => (
+                  <div key={idx}>
+                    <CustomCard radius={50} className="w-full aspect-square">
+                      <Skeleton className="size-full" />
+                    </CustomCard>
+                    <Skeleton className="h-3 w-[90%] mt-2 mx-auto" />
+                  </div>
+                ))}
+
+              {/* Loaded State */}
+              {!loading &&
+                !error &&
+                profiles.map((item, idx) => (
+                  <div
+                    key={idx}
+                    className="text-center cursor-pointer"
+                    onClick={() => {
+                      const status = item.friend_request_status;
+                      if (status === undefined || status === "accepted") {
+                        navigate(`/activity/${item.id}`);
+                      }
+                    }}
+                  >
+                    <CustomCard radius={50} className="w-full aspect-square">
+                      <img
+                        className="size-full aspect-square object-cover"
+                        src={item.profile_pic || AvtarImg}
+                        alt={item.username}
+                        loading="lazy"
+                        onError={(e) => {
+                          e.currentTarget.onerror = null;
+                          e.currentTarget.src = AvtarImg;
+                        }}
+                      />
+                    </CustomCard>
+                    <div className="overflow-hidden mt-2">
+                      <h3 className="text-xs font-medium">
+                        {item.full_name.split(" ")[0]}
+                      </h3>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </CustomCard>
+      </div>
     </PageLayout>
   );
 };
